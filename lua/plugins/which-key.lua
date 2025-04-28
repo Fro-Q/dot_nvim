ae823b98930beec33b1e86a24f6e73738d016941
Fro-Q <froqqq@outlook.com>
2025-04-28 10:56:29 +0800

    feat: support gitsign in which-key

---
+++ lua/plugins/which-key.lua
@@ -0,0 +1,17 @@
+return {
+  {
+    "folke/which-key.nvim",
+    event = "VeryLazy",
+    init = function()
+      vim.o.timeout = true
+      vim.o.timeoutlen = 500
+    end,
+    opts = {
+      spec = {
+        { "<leader>f", desc = "Fuzzy Finder" },
+        { "<leader>d", desc = "Diagnostics" },
+        { "<leader>h", desc = "Gitsigns" },
+      },
+    },
+  },
+}
