df69ebf79dfc9885f0bbca0a0fc0abec4cb176b5
Fro-Q <froqqq@outlook.com>
2025-05-05 01:04:24 +0800

    feat: remove neovide configs

--- lua/options.lua
+++ lua/options.lua
@@ -12,19 +12,19 @@ opt.termguicolors = true
 opt.wrap = false
 
 -- neovide options
-g.neovide_opacity = 0.95
-g.neovide_window_blurred = true
-g.neovide_floating_shadow = true
-
-g.neovide_floating_blur_amount_x = 10.0
-g.neovide_floating_blur_amount_y = 10.0
-
-g.neovide_floating_corner_radius = 0.6
-g.neovide_show_border = true
-
-g.neovide_cursor_unfocused_outline_width = 0.08
-g.neovide_cursor_smooth_blink = true
-
-g.neovide_cursor_vfx_mode = { "pixiedust" }
-
-g.neovide_cursor_animation_length = 0.08
+-- g.neovide_opacity = 0.95
+-- g.neovide_window_blurred = true
+-- g.neovide_floating_shadow = true
+--
+-- g.neovide_floating_blur_amount_x = 10.0
+-- g.neovide_floating_blur_amount_y = 10.0
+--
+-- g.neovide_floating_corner_radius = 0.6
+-- g.neovide_show_border = true
+--
+-- g.neovide_cursor_unfocused_outline_width = 0.08
+-- g.neovide_cursor_smooth_blink = true
+--
+-- g.neovide_cursor_vfx_mode = { "pixiedust" }
+--
+-- g.neovide_cursor_animation_length = 0.08
