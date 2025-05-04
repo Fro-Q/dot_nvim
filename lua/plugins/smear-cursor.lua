b4a5ce47cfbb2cd7ea840cc6af8b62dd7f753f87
Fro-Q <froqqq@outlook.com>
2025-05-05 01:05:17 +0800

    feat(plugins): smear-cursor

---
+++ lua/plugins/smear-cursor.lua
@@ -0,0 +1,14 @@
+return {
+  {
+    "sphamba/smear-cursor.nvim",
+    lazy = false,
+    opts = {
+      -- cursor_color = "#ff8800",
+      stiffness = 0.8,
+      trailing_stiffness = 0.5,
+      stiffness_insert_mode = 0.6,
+      trailing_stiffness_insert_mode = 0.5,
+      distance_stop_animating = 0.5,
+    },
+  },
+}
